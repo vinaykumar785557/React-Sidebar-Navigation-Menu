984dc73e20eff038fcab9cf2af3aeff0dd668f5d
vinaykumar785557 <c0785557@mylambton.ca>
2022-01-19 22:43:35 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./Pages/Home";
+import Products from "./Pages/Products";
+import Reports from "./Pages/Reports";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/reports" component={Reports} />
+          <Route path="/products" component={Products} />
+          <Route path="/team" component={Home} />
+          <Route path="/messages" component={Home} />
+          <Route path="/support" component={Home} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
 export default App;
+
+// alt shift downarrow
